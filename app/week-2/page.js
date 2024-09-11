c3fe2fece87e5a47a3a385775f2bb3f37f898451
Akshar Patel <patelakshar1483@gmail.com>
2024-09-11 17:20:29 -0600

    cprg-306 assignment
    completed the assignment with new code of root page and week 2 link tag

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import page from "./student-info";
+import StudentInfo from "./student-info";
+export default function Page() {
+    return (
+        <headers>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+        </headers>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Akshar Patel</h1>
+            <h1>
+                <Link href="https://github.com/Akshar1483" >https://github.com</Link>
+            </h1>
+        </main>
+       
+    );
+}
